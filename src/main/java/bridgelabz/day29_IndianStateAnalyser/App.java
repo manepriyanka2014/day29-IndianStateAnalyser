1a98b071f5e24c6e4c4ea4f6caebb39588274a1a
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-11-09 21:39:56 +0530

    Added : Test case for Indian State Census Analyser

---
+++ src/main/java/bridgelabz/day29_IndianStateAnalyser/App.java
@@ -0,0 +1,13 @@
+package bridgelabz.day29_IndianStateAnalyser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
